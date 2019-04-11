a0f909123c6e72e7e8ec008c6992774f2bd43d49
Marcel Kristensen <36763180+Razhork@users.noreply.github.com>
2019-04-11 12:39:42 +0200

    Add project files.

---
+++ UDPNumberReceiver/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPNumberReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creates a UdpClient for reading incoming data.
+            UdpClient udpServer = new UdpClient(9999);
+
+            //Creates an IPEndPoint to record the IP Address and port number of the sender.  
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 9999);
+
+            try
+            {
+                // Blocks until a message is received on this socket from a remote host (a client).
+                Console.WriteLine("Server is blocked");
+                while (true)
+                {
+                    Byte[] receiveBytes = udpServer.Receive(ref RemoteIpEndPoint);
+                    //Server is now activated");
+
+                    string receivedData = Encoding.ASCII.GetString(receiveBytes);
+       
+
+                    Console.WriteLine(receivedData);
+                    //Console.WriteLine("Received from: " + clientName.ToString() + " " + text.ToString());
+                    Console.WriteLine("This message was sent from " +
+                                      RemoteIpEndPoint.Address.ToString() +
+                                      " on their port number " +
+                                      RemoteIpEndPoint.Port.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
